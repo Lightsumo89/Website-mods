51a9224693f551639007c1ffd623a227469fdc28
Mio Mahoney <mmahone5@oswego.edu>
2024-09-27 22:05:57 -0400

    Added suffix on date

--- src/lib/clock.svelte
+++ src/lib/clock.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import dayjs from 'dayjs';
-
+	import advancedFormat from 'dayjs/plugin/advancedFormat.js';
+	dayjs.extend(advancedFormat);
 	let currentDate: String;
 	let currentTime: String;
 	time();
 	setInterval(time, 1000);
 	function time() {
 		currentTime = dayjs().format('h:mm:ssa');
-		currentDate = dayjs().format('dddd, MMMM D');
+		currentDate = dayjs().format('dddd, MMMM Do');
 	}
 </script>
 
