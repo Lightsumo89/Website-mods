7314acc6469b39a9af04e36f4414c06710e0a65a
Mio Mahoney <mmahone5@oswego.edu>
2024-11-12 15:15:11 -0500

    updated again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,8 +19,8 @@
 <div
 	class="bg-gradient-to-br from-[#B190BA] to-[#E8B595] h-screen w-screen items-center space-y-10 justify-center flex"
 >
-	<div class="text-6xl bg-gradient-to-br from-[#B190BA] to-[#E8B595] bg-clip-text">
-		<div class=" flex flex-col justify-center">
+	<div class="flex justify-center items-center">
+		<div class=" flex flex-col text-5xl">
 			<div
 				class="rounded-lg backdrop-blur-xl m-[--margin] p-[--padding] bg-[--bcolor] text-[#C39CAE]/70"
 			>
@@ -29,15 +29,15 @@
 				</div>
 			</div>
 			<div
-				class="rounded-lg backdrop-blur-xl m-[--margin] p-[--padding] bg-[--bcolor] text-fair-pink-100/70"
+				class="rounded-lg backdrop-blur-xl m-[--margin] p-[--padding] bg-[--bcolor] text-[#C39CAE]/70"
 			>
-				<svelte:component this={components[1]} />
+				<div class="bg-gradient-to-br from-[#B190BA] to-[#E8B595] bg-clip-text">
+					<svelte:component this={components[1]} />
+				</div>
 			</div>
 		</div>
-		<div class="bg-[--bcolor] rounded-lg flex justify-center m-[--margin]">
-			<div
-				class="text-6xl rounded-lg backdrop-blur-xl m-15 p-[--padding] bg-[--bcolor] bg-gradient-to-br from-[#B190BA] to-[#E8B595] bg-clip-text text-fair-pink-50/25"
-			>
+		<div class="bg-[--bcolor] rounded-lg p-9 h-max text-[#C39CAE]/70">
+			<div class="bg-gradient-to-br from-[#B190BA] to-[#E8B595] bg-clip-text">
 				<svelte:component this={components[2]} />
 			</div>
 		</div>
